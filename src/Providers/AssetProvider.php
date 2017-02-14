86a42670d2d30ef7d35eefeaf2329f37819cfa97
Edwin Hoksberg <mail@edwinhoksberg.nl>
2017-02-14 14:49:17 +0100

    Set asset base url + use env helper instead of getenv

--- src/Providers/AssetProvider.php
+++ src/Providers/AssetProvider.php
@@ -4,14 +4,16 @@ namespace App\Providers;
 
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 
 class AssetProvider implements ProviderInterface
 {
     public static function register(Application $app)
     {
         $app->register(new AssetServiceProvider(), [
+            'assets.base_urls' => Request::createFromGlobals()->getSchemeAndHttpHost(),
             'assets.version_format' => '%s?v=%s',
-            'assets.version' => getenv('APP_VERSION'),
+            'assets.version' => env('APP_VERSION'),
         ]);
     }
 }
