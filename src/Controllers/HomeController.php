e465100a56eb9b81b62361900a74693dc16fb77c
Edwin Hoksberg <mail@edwinhoksberg.nl>
2017-02-17 11:20:54 +0100

    Remove testing code from home controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,25 +3,12 @@
 namespace App\Controllers;
 
 use App\Application;
-use App\Entities\Product;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomeController
 {
     public function index(Request $request, Application $app)
     {
-        $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(19.99);
-        $product->setDescription('Awesome description!');
-
-        $em = $app['orm.em'];
-        $em->persist($product);
-
-        $em->flush();
-
-        var_dump($product->getId());exit;
-
         return $app->renderView('home/index.twig');
     }
 }
