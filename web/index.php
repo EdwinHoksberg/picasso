5fae9a8cae77926da9d0a76bd49239e92f0b6575
Edwin Hoksberg <mail@edwinhoksberg.nl>
2017-02-18 19:30:36 +0100

    Add some informational comments

--- web/index.php
+++ web/index.php
@@ -2,10 +2,12 @@
 
 use App\Providers;
 
+// Define a root directory constant
 if (!defined('ROOT_DIR')) {
     define('ROOT_DIR', dirname(__DIR__));
 }
 
+// Require composer autoload
 require_once ROOT_DIR.'/vendor/autoload.php';
 
 // Make php development server compatible
@@ -16,12 +18,15 @@ if (php_sapi_name() === 'cli-server') {
     }
 }
 
+// Load configuration from the .env file
 $dotenv = new \Dotenv\Dotenv(ROOT_DIR);
 $dotenv->load();
 
+// Create the application and set debug mode
 $app = new App\Application();
 $app['debug'] = env('APP_DEBUG', false);
 
+// Define all providers to be loaded
 $providers = [
     Providers\SessionProvider::class,
     Providers\TwigProvider::class,
@@ -36,16 +41,21 @@ $providers = [
     Providers\WhoopsProvider::class,
 ];
 
+// Load all previously defined providers
 foreach ($providers as $provider) {
     $provider::register($app);
 }
 
+// Load middleware
 require ROOT_DIR.'/src/middleware.php';
 
+// Load application routes
 require ROOT_DIR.'/src/routes.php';
 
+// If APP_BOOTSTRAP was defined, we return the app object, this is used for testing and cli tools
 if (defined('APP_BOOTSTRAP')) {
     return $app;
 }
 
+// Run the application
 $app->run();
